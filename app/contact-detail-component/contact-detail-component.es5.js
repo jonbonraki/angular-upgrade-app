8a6b3d0b511a8119f514a2dacdf059e7d9e0508e
jonbonraki <raj123om@gmail.com>
2016-09-18 18:46:17 -0700

    intial commit

--- app/contact-detail-component/contact-detail-component.es5.js
+++ app/contact-detail-component/contact-detail-component.es5.js
@@ -4,19 +4,4 @@ angular
     $routeProvider.when('/contact/:id', {
       template: '<contact-detail-component><contact-detail-component>'
     });
-  })
-  .component('contactDetailComponent', {
-    templateUrl: 'app/contact-detail-component/contact-detail-component.html',
-    controller: function (contactsService, $routeParams) {
-
-      this.$onInit = function () {
-        this.contact = contactsService.getContact($routeParams.id);
-        this.toggleCaption(false);
-      };
-
-      this.toggleCaption = function (closed) {
-        this.zippyCaption = closed ? 'Show address' : 'Hide address';
-      };
-
-    }
-});
+  });
---
+++ app/downgrades.ts
@@ -0,0 +1,15 @@
+import {upgradeAdapter} from './upgrade-adapter';
+import {ContactsListItemComponent} from './contacts-list-item-component/contacts-list-item-component';
+import {ContactDetailComponent} from './contact-detail-component/contact-detail-component';
+import {ContactsService} from './contacts-service/contacts-service';
+
+declare let angular:any;
+
+angular.module('contacts-service', [])
+       .service('contactsService', upgradeAdapter.downgradeNg2Provider(ContactsService));
+
+angular.module('contacts-list-item-component', [])
+       .directive('contactsListItemComponent', upgradeAdapter.downgradeNg2Component(ContactsListItemComponent));
+
+angular.module('contact-detail-component')
+       .directive('contactDetailComponent', upgradeAdapter.downgradeNg2Component(ContactDetailComponent));
